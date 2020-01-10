69b7e892f94459c90a956804db183c6ac65a8b0f
Server-InvidA <57058961+Server-InvidA@users.noreply.github.com>
2020-01-10 22:13:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.on('ready', function () {
 })
 
 bot.on('message', function (message) {
-	if (message.content === '/help') {
+	/*if (message.content === '/help') {
 		Help_BOOK.send("Liste des commandes du serveur discord")
 		let cmdEmbed = new Discord.RichEmbed()
 			.setColor("#0EB30E")
@@ -16,13 +16,13 @@ bot.on('message', function (message) {
 		Help_BOOK.send(cmdEmbed)
 	}
 
-	/*if (message.content === '?grades-bourgeois') {
+	if (message.content === '?grades-bourgeois') {
 		Help_BOOK.send("Liste des commandes du serveur discord")
 		let gradesEmbed = new Discord.RichEmbed()
 			.setColor("#0EB30E")
 			.addField("Grade Joueur :", "Affiche la liste des commandes du serveur discord")
 		Help_BOOK.send(gradesEmbed)
-	}*/
+	}
 
 	if (message.content === '?grades-infos') {
 		Help_BOOK.send("Liste des grades en jeu")
@@ -31,7 +31,7 @@ bot.on('message', function (message) {
 			.addField("Bourgeois", "V.I.P")
 			.addField("V.I.P+", "Youtuber")
 		Help_BOOK.send(gradesEmbed)
-	}
+	}*/
 })
 
 bot.login(process.env.TOKEN)
